94872a5fba6ac443f92c63bbbd7e4e77a6a714b8
Pratika NG <pratika.05ng@gmail.com>
2025-07-17 12:29:21 +0530

    using get with postman and fixing bugs/errors using middleware

--- five_FormsAndImages/index.js
+++ five_FormsAndImages/index.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const app = express()
 
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:"true"})) 
+//{extended:"true"} -> handles everything thats extended in data {"fullname":{"firstname":"pratika","lastname":"ng"}}
+
+app.get('/myget',(req,res)=>{
+    console.log("req.body: ",req.body)
+    res.status(200).json(req.body)
+})
+
+
+
 app.listen(4000,()=>{console.log("App is listening to port 4000")})
