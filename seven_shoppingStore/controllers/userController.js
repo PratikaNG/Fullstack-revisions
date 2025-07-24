e8f6b08777edd5b12632c5ec548be4d0ab773d10
Pratika NG <pratika.05ng@gmail.com>
2025-07-24 12:58:23 +0530

    user signup, cookies and db connection

--- seven_shoppingStore/controllers/userController.js
+++ seven_shoppingStore/controllers/userController.js
@@ -1,5 +1,27 @@
+// 1. wrap the controller into BigPromise
+// 2. check the requirements and bring in custom error is required
 const BigPromise = require("../middlewares/bigPromise")
+const customError = require("../utils/customError")
+const User = require("../models/user")
+const cookieToken = require("../utils/cookieToken")
 
 exports.signup = BigPromise(async(req,res,next)=>{
-     res.send("Signed up")
+     const {name, email,password} = req.body
+
+     if(!email || !name || !password){
+          // next is used since it is a util
+          return next(new customError("Name, email, password are required.",400))
+          // can use classic error as below, it only takes error message.
+          // return next(new Error("Please end email.")) 
+     }
+
+     // 3. create a user that takes name, email and password
+     const user = await User.create({
+          name,
+          email,
+          password
+     })
+
+     // 4. Send a cookie value since the user has successfully registered
+     cookieToken(user,res)
 })
--- seven_shoppingStore/index.js
+++ seven_shoppingStore/index.js
@@ -1,5 +1,9 @@
 const app = require('./app')
+const connectWithDB = require('./config/database')
 require("dotenv").config()
 const {PORT} = process.env
+
+// connect with db before the server starts
+connectWithDB();
 
 app.listen(PORT,()=>{console.log(`Server is running at the port: ${PORT}`)})
