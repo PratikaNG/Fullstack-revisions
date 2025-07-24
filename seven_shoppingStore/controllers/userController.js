d732abd552b6e3e7691b869ebcf2b84d7e4672ac
Pratika NG <pratika.05ng@gmail.com>
2025-07-24 14:51:15 +0530

    testing usersignup and image upload

--- seven_shoppingStore/controllers/userController.js
+++ seven_shoppingStore/controllers/userController.js
@@ -4,9 +4,13 @@ const BigPromise = require("../middlewares/bigPromise")
 const customError = require("../utils/customError")
 const User = require("../models/user")
 const cookieToken = require("../utils/cookieToken")
+const cloudinary = require("cloudinary").v2
 
 exports.signup = BigPromise(async(req,res,next)=>{
-     const {name, email,password} = req.body
+     // let result ;
+     
+
+     const {name,email,password} = req.body
 
      if(!email || !name || !password){
           // next is used since it is a util
@@ -15,11 +19,27 @@ exports.signup = BigPromise(async(req,res,next)=>{
           // return next(new Error("Please end email.")) 
      }
 
+     if(!req.files){
+          return new customError("Photo is required for signup",400)
+     }
+     let
+          file = req.files.photo;  
+           //frontend should know that they should send the images on "photo" field
+           const result = await cloudinary.uploader.upload(file.tempFilePath,{
+               folder:"users",
+               width:150,
+               crop:"scale"
+          })
+
      // 3. create a user that takes name, email and password
      const user = await User.create({
           name,
           email,
-          password
+          password,
+          photo:{
+               id:result.public_id,
+               secure_url: result.secure_url
+          }
      })
 
      // 4. Send a cookie value since the user has successfully registered
--- seven_shoppingStore/index.js
+++ seven_shoppingStore/index.js
@@ -2,8 +2,18 @@ const app = require('./app')
 const connectWithDB = require('./config/database')
 require("dotenv").config()
 const {PORT} = process.env
+const cloudinary = require("cloudinary").v2
 
-// connect with db before the server starts
+
+//2. connect with db before the server starts
 connectWithDB();
+
+// 3. cloudinary connect
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
+
 
 app.listen(PORT,()=>{console.log(`Server is running at the port: ${PORT}`)})
